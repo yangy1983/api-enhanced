eacfc42544470d60c3e19adc4adf1b7f8a1aab7b
yangy1983 <271380391@qq.com>
2026-06-29 16:04:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-require('./app.js')
+javascript
+// index.js - Vercel serverless entry point
+const { constructServer } = require('./server');
+
+let cachedApp;
+
+module.exports = asyns (req, res) => {
+  if (!cachedApp){
+    const app = await constructServer);
+    cachedApp =app;
+  }
+  return cachedApp(req, res);
+};
